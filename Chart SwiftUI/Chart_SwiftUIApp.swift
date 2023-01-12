1074d14c11e2a3e8b648c9428543ab234b6ab568
Chithian Samony <chithiansamony@gmail.com>
2023-01-12 13:34:11 +0700

    Initial Commit

---
+++ Chart SwiftUI/Chart_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Chart_SwiftUIApp.swift
+//  Chart SwiftUI
+//
+//  Created by Samony Chithian on 12/1/23.
+//
+
+import SwiftUI
+
+@main
+struct Chart_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
